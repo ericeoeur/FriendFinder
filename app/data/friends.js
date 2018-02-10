3154ce628e6a901aa74c48878347ff1abddf4088
Eric Oeur <ericeoeur@gmail.com>
2018-02-10 08:07:30 -0600

    this is the final commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,19 +3,52 @@
 
 let friends = [
     {
-        name: "Matt",
-        photo: "Partner",
-        answers: ""
+        "name": "Matt",
+        "photo": "Partner",
+        "answers": [
+            "4",
+            "2",
+            "3",
+            "2",
+            "1",
+            "5",
+            "4",
+            "2",
+            "1",
+            "3"
+            ]
     },
     {
-        name: "Vivi",
-        photo: "Best Friend",
-        answers: ""
+        "name": "Vivi",
+        "photo": "Best Friend",
+        "answers": [
+            "4",
+            "1",
+            "1",
+            "2",
+            "3",
+            "5",
+            "2",
+            "1",
+            "3",
+            "2"
+            ]
     },
     {
-        name: "Siena",
-        photo: "Best Friend",
-        answers: ""
+        "name": "Siena",
+        "photo": "Best Friend",
+        "answers": [
+            "1",
+            "2",
+            "2",
+            "5",
+            "3",
+            "2",
+            "2",
+            "3",
+            "4",
+            "1"
+            ]
     }
 ];
 
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -7,9 +7,11 @@ A POST routes /api/friends. This will be used to handle incoming survey results.
 
 
 */
-
+let path = require('path');
 let friendData = require("../app/data/friends"); 
+//let friends = friendData.friends;
 
+var totalDifference = 0;
 let testArray = [
     {
         name: "Matthew",
@@ -30,7 +32,55 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        friendData.push(req.body);
+
+        let myMatch = {
+			name: "",
+			image: "",
+			difference: 9999
+        };
+
+        	var currentHighScore;
+		var arrayOfDifferences = [];
+		let userData 	= req.body;
+		let name 	= userData.name;
+		let photo 	= userData.photo;
+		let userScores 	= userData.answers;
+
+        
+
+        console.log(userData); 
+
+        //loop through the friends data to get each friends scores
+		for(var i = 0; i < friendData.length-1; i++){
+			//console.log(friends[i].name);
+			totalDifference = 0;
+
+			//loop through that friends score
+			for(var j = 0; j < friendData[i].answers.length; j++){
+				// We calculate the absolute difference
+				//totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendData[i].answers[j]));
+                console.log("test"); 
+                // if it's less than the current best match then this is new best match
+				
+				//console.log(friends[i].photo);
+				if (totalDifference <= myMatch.difference){
+					// set the values 
+					myMatch.name = friendData[i].name;
+					myMatch.image = friendData[i].photo;
+					myMatch.difference = totalDifference;
+                }
+                
+			}
+		}
+        
+        
+        res.json(myMatch); 
+
+
+
+
+        //this pushes data to the api, only works when its by itself
+        //friendData.push(req.body);
 
     });
 
